446790344753fc042c467c062c7bc84f57ffc091
tacores <buttix@gmail.com>
2015-11-08 20:52:03 +0900

    deal with pylint

--- tests.py
+++ tests.py
@@ -2,237 +2,240 @@ import unittest
 import ydl_helper
 
 setting = {'download_playlists' : []}
-filenames = [[],[]]
+filenames = [[], []]
 platform = 'darwin'
 
 class SettingStub(ydl_helper.SettingProvider):
-	def get_download_settings(self):
-		return setting
-	def save_download_settings(self,settings):
-		self.arg = settings
-	def get_save_arg(self):
-		return self.arg
+    def get_download_settings(self):
+        return setting
+    def save_download_settings(self, settings):
+        self.arg = settings
+    def get_save_arg(self):
+        return self.arg
 
 class FsStub(ydl_helper.FileSystemProvider):
-	def __init__(self):
-		self.index = 0
-	def get_filenames_pettern_matched(self,pettern):
-		self.arg = pettern
-		self.index += 1
-		return filenames[self.index - 1]
-	def get_pettern_arg(self):
-		return self.arg
+    def __init__(self):
+        self.index = 0
+    def get_filenames_pettern_matched(self, pettern):
+        self.arg = pettern
+        self.index += 1
+        return filenames[self.index - 1]
+    def get_pettern_arg(self):
+        return self.arg
 
 class SubProcessSpy(ydl_helper.SubProcessCaller):
-	def __init__(self):
-		self.times = 0
-	def call(self,args):
-		self.times += 1
-		self.args = args
-	def get_args(self):
-		return self.args
-	def get_call_times(self):
-		return self.times
+    def __init__(self):
+        self.times = 0
+    def call(self, args):
+        self.times += 1
+        self.args = args
+    def get_args(self):
+        return self.args
+    def get_call_times(self):
+        return self.times
 
 class SystemStub(ydl_helper.SystemProvider):
-	def get_platform(self):
-		return platform
-		
+    def get_platform(self):
+        return platform
+
 class YdlHelperTest(unittest.TestCase):
-	def setUp(self):
-		global setting,filenames,args
-		setting = {'download_playlist' : []}
-		filenames = [[],[]]
-		platform = 'darwin'
+    def setUp(self):
+        global setting, filenames, args, platform
+        setting = {'download_playlist' : []}
+        filenames = [[], []]
+        platform = 'darwin'
 
-		self.setting_stub = SettingStub()
-		self.fs_stub = FsStub()
-		self.subp_spy = SubProcessSpy()
-		self.sys_stub = SystemStub()
-		self.sut = ydl_helper.YdlPlaylistHelper(self.setting_stub, self.fs_stub, self.subp_spy, self.sys_stub)
-		
-	def test_subprocess_call_with_args(self):
-		global setting
-		setting = {"download_playlists" : [ {
-		"url" : "https://ydl-url",
-		"--playlist-start" : 10,
-		"file_name_pettern" : "*pettern*"
-		} ] }
+        self.setting_stub = SettingStub()
+        self.fs_stub = FsStub()
+        self.subp_spy = SubProcessSpy()
+        self.sys_stub = SystemStub()
+        self.sut = ydl_helper.YdlPlaylistHelper(self.setting_stub, self.fs_stub,
+                                                self.subp_spy, self.sys_stub)
 
-		self.sut.download_all_playlists()
+    def test_subprocess_call_with_args(self):
+        global setting
+        setting = {"download_playlists" : [{\
+                   "url" : "https://ydl-url",\
+                   "--playlist-start" : 10,\
+                   "file_name_pettern" : "*pettern*"\
+        }]}
 
-		expect = ['youtube-dl', '--playlist-start', '10', 'https://ydl-url']
-		self.assertEqual(expect, self.subp_spy.get_args())
+        self.sut.download_all_playlists()
 
-	def test_subprocess_call_with_args_windows_exe(self):
-		global setting,platform
-		setting = {"download_playlists" : [ {
-		"url" : "https://ydl-url",
-		"--playlist-start" : 10,
-		"file_name_pettern" : "*pettern*"
-		} ] }
-		platform = 'win32'	# Windows
+        expect = ['youtube-dl', '--playlist-start', '10', 'https://ydl-url']
+        self.assertEqual(expect, self.subp_spy.get_args())
 
-		self.sut.download_all_playlists()
+    def test_subprocess_call_with_args_windows_exe(self):
+        global setting, platform
+        setting = {"download_playlists" : [{\
+            "url" : "https://ydl-url",\
+            "--playlist-start" : 10,\
+            "file_name_pettern" : "*pettern*"\
+        }]}
+        platform = 'win32'    # Windows
 
-		expect = ['youtube-dl.exe', '--playlist-start', '10', 'https://ydl-url']
-		self.assertEqual(expect, self.subp_spy.get_args())
+        self.sut.download_all_playlists()
 
-	def test_subprocess_call_with_any_args(self):
-		global setting
-		setting = {"download_playlists" : [ {
-		"url" : "https://ydl-url",
-		"--playlist-start" : 10,
-		"file_name_pettern" : "*pettern*",
-		"-f" : "140"	# An user want to download as m4a format.
-		} ] }
+        expect = ['youtube-dl.exe', '--playlist-start', '10', 'https://ydl-url']
+        self.assertEqual(expect, self.subp_spy.get_args())
 
-		self.sut.download_all_playlists()
+    def test_subprocess_call_with_any_args(self):
+        global setting
+        setting = {"download_playlists" :[{\
+            "url" : "https://ydl-url",\
+            "--playlist-start" : 10,\
+            "file_name_pettern" : "*pettern*",\
+            "-f" : "140"}]}    # An user want to download as m4a format.
 
-		expect = ['youtube-dl', '--playlist-start', '10', '-f', '140', 'https://ydl-url']
-		self.assertEqual(expect, self.subp_spy.get_args())
+        self.sut.download_all_playlists()
 
-	def test_subprocess_call_empty_value_option(self):
-		global setting
-		setting = {"download_playlists" : [ {
-		"url" : "https://ydl-url",
-		"--playlist-start" : 10,
-		"file_name_pettern" : "*pettern*",
-		"-c" : ""
-		} ] }
+        expect = ['youtube-dl', '--playlist-start', '10', '-f', '140', 'https://ydl-url']
+        self.assertEqual(expect, self.subp_spy.get_args())
 
-		self.sut.download_all_playlists()
+    def test_subprocess_call_empty_value_option(self):
+        global setting
+        setting = {"download_playlists" :[{\
+            "url" : "https://ydl-url",\
+            "--playlist-start" : 10,\
+            "file_name_pettern" : "*pettern*",\
+            "-c" : ""\
+        }]}
 
-		expect = ['youtube-dl', '--playlist-start', '10', '-c', 'https://ydl-url']
-		self.assertEqual(expect, self.subp_spy.get_args())
+        self.sut.download_all_playlists()
 
-	def test_save_start_position_2files_downloaded(self):
-		global setting,filenames
-		setting = {"download_playlists" : [ {
-		"url" : "https://ydl-url",
-		"--playlist-start" : 10,
-		"file_name_pettern" : "*name*"
-		} ] }
-		filenames = [['videoname1','nextname2'],['videoname1','nextname2', 'downname3','loadname4']]
+        expect = ['youtube-dl', '--playlist-start', '10', '-c', 'https://ydl-url']
+        self.assertEqual(expect, self.subp_spy.get_args())
 
-		self.sut.download_all_playlists()
+    def test_save_start_position_2files_downloaded(self):
+        global setting, filenames
+        setting = {"download_playlists" : [{\
+            "url" : "https://ydl-url",\
+            "--playlist-start" : 10,\
+            "file_name_pettern" : "*name*"\
+        }]}
+        filenames = [['videoname1', 'nextname2'],\
+                     ['videoname1', 'nextname2', 'downname3', 'loadname4']]
 
-		expect = {"download_playlists" : [ {
-		"url" : "https://ydl-url",
-		"--playlist-start" : 12,
-		"file_name_pettern" : "*name*"
-		} ] }
-		self.assertEqual(expect, self.setting_stub.get_save_arg())
+        self.sut.download_all_playlists()
 
-	def test_not_count_uncomplete_file(self):
-		global setting,filenames
-		setting = {"download_playlists" : [ {
-		"url" : "https://ydl-url",
-		"--playlist-start" : 10,
-		"file_name_pettern" : "*name*"
-		} ] }
-		filenames = [['videoname1','nextname2'],['videoname1','nextname2', 'downname3','loadname4.part']] # .part
+        expect = {"download_playlists" : [{\
+            "url" : "https://ydl-url",\
+            "--playlist-start" : 12,\
+            "file_name_pettern" : "*name*"\
+        }]}
+        self.assertEqual(expect, self.setting_stub.get_save_arg())
 
-		self.sut.download_all_playlists()
+    def test_not_count_uncomplete_file(self):
+        global setting, filenames
+        setting = {"download_playlists" : [{\
+            "url" : "https://ydl-url",\
+            "--playlist-start" : 10,\
+            "file_name_pettern" : "*name*"\
+        }]}
+        filenames = [['videoname1', 'nextname2'],\
+                     ['videoname1', 'nextname2', 'downname3', 'loadname4.part']] # .part
 
-		expect = {"download_playlists" : [ {
-		"url" : "https://ydl-url",
-		"--playlist-start" : 11,
-		"file_name_pettern" : "*name*"
-		} ] }
-		self.assertEqual(expect, self.setting_stub.get_save_arg())
+        self.sut.download_all_playlists()
 
-	def test_get_filenames_pettern_matched(self):
-		global setting,filenames
-		setting = {"download_playlists" : [ {
-		"url" : "https://ydl-url",
-		"--playlist-start" : 10,
-		"file_name_pettern" : "*pettern*"
-		} ] }
+        expect = {"download_playlists" : [{\
+            "url" : "https://ydl-url",\
+            "--playlist-start" : 11,\
+            "file_name_pettern" : "*name*"\
+        }]}
+        self.assertEqual(expect, self.setting_stub.get_save_arg())
 
-		self.sut.download_all_playlists()
+    def test_get_filenames_pettern_matched(self):
+        global setting, filenames
+        setting = {"download_playlists" : [{\
+            "url" : "https://ydl-url",\
+            "--playlist-start" : 10,\
+            "file_name_pettern" : "*pettern*"\
+        }]}
 
-		self.assertEqual('*pettern*', self.fs_stub.get_pettern_arg())
+        self.sut.download_all_playlists()
 
-	def test_run_ydl_2times(self):
-		global setting,filenames
-		setting = {"download_playlists" : [ {
-		"url" : "https://ydl-url-1",
-		"--playlist-start" : 10,
-		"file_name_pettern" : "*pettern-1*"
-		} , {
-		"url" : "https://ydl-url-2",
-		"--playlist-start" : 20,
-		"file_name_pettern" : "*pettern-2*"
-		} ] }
-		filenames = [[],[],[],[]]
+        self.assertEqual('*pettern*', self.fs_stub.get_pettern_arg())
 
-		self.sut.download_all_playlists()
+    def test_run_ydl_2times(self):
+        global setting, filenames
+        setting = {"download_playlists" : [{\
+            "url" : "https://ydl-url-1",\
+            "--playlist-start" : 10,\
+            "file_name_pettern" : "*pettern-1*"\
+            }, {\
+            "url" : "https://ydl-url-2",\
+            "--playlist-start" : 20,\
+            "file_name_pettern" : "*pettern-2*"\
+        }]}
+        filenames = [[], [], [], []]
 
-		self.assertEqual(2, self.subp_spy.get_call_times())
+        self.sut.download_all_playlists()
 
-	def test_not_run_ydl_when_url_is_not_set(self):
-		global setting,filenames
-		setting = {"download_playlists" : [ {
-		"--playlist-start" : 10,
-		"file_name_pettern" : "*pettern-1*"
-		} ] }
+        self.assertEqual(2, self.subp_spy.get_call_times())
 
-		self.sut.download_all_playlists()
+    def test_not_run_ydl_when_url_is_not_set(self):
+        global setting, filenames
+        setting = {"download_playlists" : [{\
+            "--playlist-start" : 10,\
+            "file_name_pettern" : "*pettern-1*"\
+        }]}
 
-		self.assertEqual(0, self.subp_spy.get_call_times())
+        self.sut.download_all_playlists()
 
-	def test_run_ydl_only_once(self):
-		global setting,filenames
-		setting = {"download_playlists" : [ {
-		# url is not set.
-		"--playlist-start" : 10,
-		"file_name_pettern" : "*pettern-1*"
-		} , {
-		"url" : "https://ydl-url-2",
-		"--playlist-start" : 20,
-		"file_name_pettern" : "*pettern-2*"
-		} ] }
-		filenames = [[],[],[],[]]
+        self.assertEqual(0, self.subp_spy.get_call_times())
 
-		self.sut.download_all_playlists()
+    def test_run_ydl_only_once(self):
+        global setting, filenames
+        setting = {"download_playlists" : [{\
+            # url is not set.
+            "--playlist-start" : 10,\
+            "file_name_pettern" : "*pettern-1*"\
+            }, {\
+            "url" : "https://ydl-url-2",\
+            "--playlist-start" : 20,\
+            "file_name_pettern" : "*pettern-2*"\
+        }]}
+        filenames = [[], [], [], []]
 
-		expect = ['youtube-dl', '--playlist-start', '20', 'https://ydl-url-2']
-		self.assertEqual(expect, self.subp_spy.get_args())
-		self.assertEqual(1, self.subp_spy.get_call_times())
-		
-	def test_save_playliststart_as_integer_even_if_set_as_string(self):
-		global setting,filenames
-		setting = {"download_playlists" : [ {
-		"url" : "https://ydl-url",
-		"--playlist-start" : "10",
-		"file_name_pettern" : "*name*"
-		} ] }
-		filenames = [['videoname1','nextname2'],['videoname1','nextname2', 'downname3','loadname4']]
+        self.sut.download_all_playlists()
 
-		self.sut.download_all_playlists()
-		
-		expect = {"download_playlists" : [ {
-			"url" : "https://ydl-url",
-			"--playlist-start" : 12,
-			"file_name_pettern" : "*name*"
-		} ] }
-		self.assertEqual(expect, self.setting_stub.get_save_arg())
+        expect = ['youtube-dl', '--playlist-start', '20', 'https://ydl-url-2']
+        self.assertEqual(expect, self.subp_spy.get_args())
+        self.assertEqual(1, self.subp_spy.get_call_times())
 
-	def test_ignore_comment(self):
-		global setting
-		setting = {"download_playlists" : [ {
-		"comment" : "free description",
-		"url" : "https://ydl-url",
-		"--playlist-start" : 10,
-		"file_name_pettern" : "*pettern*"
-		} ] }
+    def test_save_playliststart_as_integer_even_if_set_as_string(self):
+        global setting, filenames
+        setting = {"download_playlists" : [{\
+            "url" : "https://ydl-url",\
+            "--playlist-start" : "10",\
+            "file_name_pettern" : "*name*"\
+        }]}
+        filenames = [['videoname1', 'nextname2'],\
+                     ['videoname1', 'nextname2', 'downname3', 'loadname4']]
 
-		self.sut.download_all_playlists()
+        self.sut.download_all_playlists()
 
-		expect = ['youtube-dl', '--playlist-start', '10', 'https://ydl-url']
-		self.assertEqual(expect, self.subp_spy.get_args())
+        expect = {"download_playlists" : [{\
+            "url" : "https://ydl-url",\
+            "--playlist-start" : 12,\
+            "file_name_pettern" : "*name*"\
+        }]}
+        self.assertEqual(expect, self.setting_stub.get_save_arg())
+
+    def test_ignore_comment(self):
+        global setting
+        setting = {"download_playlists" : [{\
+            "comment" : "free description",\
+            "url" : "https://ydl-url",\
+            "--playlist-start" : 10,\
+            "file_name_pettern" : "*pettern*"\
+        }]}
+
+        self.sut.download_all_playlists()
+
+        expect = ['youtube-dl', '--playlist-start', '10', 'https://ydl-url']
+        self.assertEqual(expect, self.subp_spy.get_args())
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
 
--- ydl_helper.py
+++ ydl_helper.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""This script makes more useful downloading youtube playlists using the youtube-dl.
+See https://github.com/tacores/ydl-helper"""
 
 import codecs
 import re
@@ -9,96 +11,129 @@ import subprocess
 import sys
 
 class YdlPlaylistHelper(object):
-	def __init__(self, setting_provider, file_system_provider,
-	subprocess_caller, system_provider):
-		self.setting_provider = setting_provider
-		self.file_system_provider = file_system_provider
-		self.subprocess_caller = subprocess_caller
-		self.system_provider = system_provider
-		
-	def download_all_playlists(self):
-		settings = self.setting_provider.get_download_settings()
-		for setting in settings["download_playlists"]:
-			try:
-				self.__parse_setting(setting)
-				before = self.__count_current_files()
-				self.__download_one_playlist(setting)
-				after = self.__count_current_files()
-				self.__update_start_position(setting, after - before)
-			except KeyError as e:
-				print(e, 'is not set.')
-		self.setting_provider.save_download_settings(settings)
-		
-	def __parse_setting(self, setting):
-		self.__url = setting["url"]
-		self.__start_from = setting["--playlist-start"]
-		self.__name_pettern = setting["file_name_pettern"]
+    """Main class of this script"""
 
-		self.__any_option_dict = {}
-		for key in setting.keys():
-			if(self.__is_optional_setting(key)):
-				self.__any_option_dict[key] = setting[key]
-				
-	def __is_optional_setting(self, key):
-		if(key=="url" or key=="--playlist-start" or key=="file_name_pettern" or key=="comment"):
-			return False
-		else:
-			return True
-				
-	def __download_one_playlist(self, setting):
-		args = self.__generate_subprocess_args(setting)
-		self.subprocess_caller.call(args)
-		
-	def __generate_subprocess_args(self, setting):
-		if self.system_provider.get_platform() == 'win32':
-			args = ['youtube-dl.exe']
-		else:
-			args = ['youtube-dl']
-		args.extend(['--playlist-start', str(self.__start_from)])
-		for k,v in self.__any_option_dict.iteritems():
-			if v != "":
-				args.extend([k,v])
-			else:
-				args.append(k)
-		args.append(self.__url)
-		return args;
-		
-	def __count_current_files(self):
-		count = 0
-		file_paths = self.file_system_provider.get_filenames_pettern_matched(self.__name_pettern)
-		for file_path in file_paths:
-			match = re.search(r"\.part$", file_path)
-			if match is None:
-				count += 1
-		return count
-		
-	def __update_start_position(self, setting, added):
-		old = int(setting["--playlist-start"])
-		setting["--playlist-start"] = old + added
+    def __init__(self, setting_provider, file_system_provider,
+                 subprocess_caller, system_provider):
+        self.setting_provider = setting_provider
+        self.file_system_provider = file_system_provider
+        self.subprocess_caller = subprocess_caller
+        self.system_provider = system_provider
+        self.__any_option_dict = {}
+
+    def download_all_playlists(self):
+        """Main class of this script"""
+        settings = self.setting_provider.get_download_settings()
+        for setting in settings["download_playlists"]:
+            try:
+                self.__parse_setting(setting)
+                before = self.__count_current_files(setting)
+                self.__download_one_playlist(setting)
+                after = self.__count_current_files(setting)
+                self.__update_start_position(setting, after - before)
+            except KeyError as key_error:
+                print(key_error, 'is not set.')
+        self.setting_provider.save_download_settings(settings)
+
+    def __parse_setting(self, setting):
+        """parse setting of a plyalist"""
+        # throw exception if necessary options are deleted.
+        setting["url"]
+        setting["--playlist-start"]
+        setting["file_name_pettern"]
+
+        self.__any_option_dict = {}
+        for key in setting.keys():
+            if self.__is_optional_setting(key):
+                self.__any_option_dict[key] = setting[key]
+
+    def __download_one_playlist(self, setting):
+        """download files in a plyalist"""
+        args = self.__generate_subprocess_args(setting)
+        self.subprocess_caller.call(args)
+
+    def __generate_subprocess_args(self, setting):
+        """generate arguments passed to youtube-dl"""
+        if self.system_provider.get_platform() == 'win32':
+            args = ['youtube-dl.exe']
+        else:
+            args = ['youtube-dl']
+        args.extend(['--playlist-start', str(setting['--playlist-start'])])
+        for k, value in self.__any_option_dict.iteritems():
+            if value != "":
+                args.extend([k, value])
+            else:
+                args.append(k)
+        args.append(setting['url'])
+        return args
+
+    def __count_current_files(self, setting):
+        """how many files of the playlist are there in a current directory now?"""
+        count = 0
+        file_paths = self.file_system_provider.\
+                get_filenames_pettern_matched(setting['file_name_pettern'])
+        for file_path in file_paths:
+            match = re.search(r"\.part$", file_path)
+            if match is None:
+                count += 1
+        return count
+
+    @staticmethod
+    def __is_optional_setting(key):
+        """is the key optional?"""
+        if key in ["url", "--playlist-start", "file_name_pettern", "comment"]:
+            return False
+        else:
+            return True
+
+    @staticmethod
+    def __update_start_position(setting, added):
+        """update --playlist-start value in memory"""
+        old = int(setting["--playlist-start"])
+        setting["--playlist-start"] = old + added
 
 class SettingProvider(object):
-	def get_download_settings(self):
-		with codecs.open('ydl_helper_list.json', 'r', 'utf-8') as f:
-			setting_dict = json.load(f)
-		return setting_dict
-		
-	def save_download_settings(self, settings):
-		with codecs.open('ydl_helper_list.json', 'w', 'utf-8') as f:
-			json.dump(settings, f, ensure_ascii=False, indent=4)
-			
+    """provide information about download settings an user edits"""
+    @staticmethod
+    def get_download_settings():
+        """provide settings"""
+        with codecs.open('ydl_helper_list.json', 'r', 'utf-8') as setting_file:
+            setting_dict = json.load(setting_file)
+        return setting_dict
+
+    @staticmethod
+    def save_download_settings(settings):
+        """save new settings"""
+        with codecs.open('ydl_helper_list.json', 'w', 'utf-8') as setting_file:
+            json.dump(settings, setting_file, ensure_ascii=False, indent=4)
+
 class FileSystemProvider(object):
-	def get_filenames_pettern_matched(self, pettern):
-		return glob.glob(pettern);
-		
+    """provide information about the file system"""
+    @staticmethod
+    def get_filenames_pettern_matched(pettern):
+        """how many files that are matched with the pettern?"""
+        return glob.glob(pettern)
+
 class SubProcessCaller(object):
-	def call(self, args):
-		subprocess.call(args, shell=True)
+    """wrapper of the subprocess class"""
+    @staticmethod
+    def call(args):
+        """execute the youtube-dl"""
+        subprocess.call(args, shell=True)
 
 class SystemProvider(object):
-	def get_platform(self):
-		return sys.platform
+    """provide information about the system"""
+    @staticmethod
+    def get_platform():
+        """what platform this script runs on?"""
+        return sys.platform
+
+def main():
+    """main"""
+    helper = YdlPlaylistHelper(SettingProvider(), FileSystemProvider(),
+                               SubProcessCaller(), SystemProvider())
+    helper.download_all_playlists()
 
 if __name__ == '__main__':
-	helper = YdlPlaylistHelper(SettingProvider(), FileSystemProvider(), SubProcessCaller(), SystemProvider())
-	helper.download_all_playlists()
-	
+    main()
+
